6831733ac3f1759fe23af5ba0c38920e0c9078fe
Ingmar Stapel <github@stapel-ingmar.de>
2017-10-29 04:24:37 +0100

    Create feinstaub_start.sh

---
+++ feinstaub_start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# /bin/sleep 10
+# Now I am using the gpsd auto start config.
+# gpsd -b /dev/ttyACM0 -F /var/run/gpsd.sock -G >> /home/pi/feinstaub/gpsd_daemon.log 2>&1 &
+/bin/sleep 10
+python /home/pi/feinstaub/web_feinstaub.py >> /home/pi/feinstaub/feisntaub_start_prozess.log 2>&1 &
