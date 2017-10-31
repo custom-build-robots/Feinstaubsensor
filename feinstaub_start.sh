89828b5178077da5a41d374df8343248e1a185f5
Ingmar Stapel <github@stapel-ingmar.de>
2017-10-31 08:51:44 +0100

    Update feinstaub_start.sh
    Changes the directory from /home/pi/feinstaub -> /home/pi/Feinstaubsensor

--- feinstaub_start.sh
+++ feinstaub_start.sh
@@ -3,4 +3,4 @@
 # Now I am using the gpsd auto start config.
 # gpsd -b /dev/ttyACM0 -F /var/run/gpsd.sock -G >> /home/pi/feinstaub/gpsd_daemon.log 2>&1 &
 /bin/sleep 10
-python /home/pi/feinstaub/web_feinstaub.py >> /home/pi/feinstaub/feisntaub_start_prozess.log 2>&1 &
+python /home/pi/Feinstaubsensor/web_feinstaub.py >> /home/pi/Feinstaubsensor/feisntaub_start_prozess.log 2>&1 &
